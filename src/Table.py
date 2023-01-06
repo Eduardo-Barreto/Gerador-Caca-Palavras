8ff6068a36e905279463e9b7646b93bfc01751e6
Eduardo Barreto <eduardopontobarreto@gmail.com>
2023-01-05 22:54:52 -0300

    feat: add word in table

--- src/Table.py
+++ src/Table.py
@@ -1,3 +1,13 @@
+from Word import Word
+
+orientation_increment_map = {
+    'HORIZONTAL': (0, 1),
+    'VERTICAL': (1, 0),
+    'DIAGONAL_LEFT': (1, -1),
+    'DIAGONAL_RIGHT': (1, 1)
+}
+
+
 class Table:
     '''
     Classe que representa um caça palavras
@@ -18,6 +28,25 @@ class Table:
         self.table = [['0' for i in range(rows)] for j in range(cols)]
         self.rows = rows
         self.cols = cols
+
+    def add_word(self, word: Word) -> None:
+        '''
+        Adiciona uma palavra ao caça palavras
+
+        Parâmetros:
+        ----------
+        word: Word
+            Palavra a ser adicionada
+        '''
+        # TODO: Adicionar suporte a palavras invertidas
+        # TODO: Adicionar validação para verificar se a palavra cabe
+        # TODO: Adicionar validação para verificar se a palavra não se sobrepõe a outra
+        row, col = word.position
+
+        for letter in word.text:
+            self.table[row][col] = letter
+            row += orientation_increment_map[word.orientation][0]
+            col += orientation_increment_map[word.orientation][1]
 
     def show(self) -> None:
         '''
--- src/main.py
+++ src/main.py
@@ -1,6 +1,31 @@
-from src.Table import Table
+from Table import Table
+from Word import Word
 
-table = Table()
+table = Table(15, 15)
 
-for row in table.table:
-    print(row)
+horizontal = Word(
+    text='HORIZONTAL',
+    position=(0, 0),
+    orientation='HORIZONTAL'
+)
+vertical = Word(
+    text='VERTICAL',
+    position=(1, 0),
+    orientation='VERTICAL'
+)
+diagonal_right = Word(
+    text='DIAGONAL_RIGHT',
+    position=(1, 1),
+    orientation='DIAGONAL_RIGHT'
+)
+diagonal_left = Word(
+    text='LEFT',
+    position=(10, 8),
+    orientation='DIAGONAL_LEFT'
+)
+
+table.add_word(horizontal)
+table.add_word(vertical)
+table.add_word(diagonal_right)
+table.add_word(diagonal_left)
+table.show()
